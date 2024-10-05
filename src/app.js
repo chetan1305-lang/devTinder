16491a21a35d25ee884778b186cabd29f866a9a3
Pritam Narkhede <pritamnarkhede78@gmail.com>
2024-10-05 18:55:01 +0530

    added middleware

--- src/app.js
+++ src/app.js
@@ -3,32 +3,17 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res, next) => {
-    console.log("Handling the route user");
-    //res.send("Response!!");
-    next();
-    
-},
-(req, res, next) => {
-    console.log("Handling the route user 2 !!!");
-    //res.send("2nd Response");
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 3 !!!");
-    //res.send("3nd Response");
-    next();
-},
-(req, res, next) => {
-    console.log("Handling the route user 4 !!!");
-    //res.send("4th Response");
-    next();
-},
-(req, res) => {
-    console.log("Handling the route user 5 !!!");
-    res.send("5th Response");
-}
-);
+const { adminAuth } = require("./middleware/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user", (req, res) => {
+    res.send("User dat sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All data Sent");
+})
 
 
 app.listen(3000, () => {
