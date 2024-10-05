87288b50bff5819768041d2e39935fdb9926b48a
Pritam Narkhede <pritamnarkhede78@gmail.com>
2024-10-05 06:38:41 +0530

    exploring get,put,post pathch methods

--- src/app.js
+++ src/app.js
@@ -3,20 +3,21 @@ const express = require("express");
 
 const app = express();
 
-
-
-// app.use("", (req, res) => {
-//     res.send("Hello from Dashboard dvsbsvsb");
+// app.use("/hello/2", (req, res) => {
+//     res.send("Hello hello hellooooo");
 // });
 
-
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hellooooo");
-});
+// app.use("/hello", (req, res) => {
+//     res.send("abra ka dabra");
+// });
 
 
 app.use("/test", (req, res) => {
     res.send("Namaste from Chetan!");
+});
+
+app.get("/user", (req, res) => {
+    res.send({firstName: "Chetan", lastName: "Kirange"});
 });
 
 app.listen(3000, () => {
