723e553eac3fa01478b30e6c08aa9957a8b82c85
Pritam Narkhede <pritamnarkhede78@gmail.com>
2024-10-05 09:14:58 +0530

    paly with route and error

--- src/app.js
+++ src/app.js
@@ -3,22 +3,33 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/hello/2", (req, res) => {
-//     res.send("Hello hello hellooooo");
-// });
+app.use("/user", (req, res, next) => {
+    console.log("Handling the route user");
+    //res.send("Response!!");
+    next();
+    
+},
+(req, res, next) => {
+    console.log("Handling the route user 2 !!!");
+    //res.send("2nd Response");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 3 !!!");
+    //res.send("3nd Response");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 4 !!!");
+    //res.send("4th Response");
+    next();
+},
+(req, res) => {
+    console.log("Handling the route user 5 !!!");
+    res.send("5th Response");
+}
+);
 
-// app.use("/hello", (req, res) => {
-//     res.send("abra ka dabra");
-// });
-
-
-app.use("/test", (req, res) => {
-    res.send("Namaste from Chetan!");
-});
-
-app.get("/user", (req, res) => {
-    res.send({firstName: "Chetan", lastName: "Kirange"});
-});
 
 app.listen(3000, () => {
     console.log("Server Successfully listening on Port 3000 .......")
